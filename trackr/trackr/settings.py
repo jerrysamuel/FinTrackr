77d261f6546dc77818ba53eb8a2462edea56762f
jerrysamuel81 <jerrysamuel81@gmail.com>
2025-12-20 08:57:17 +0100

    made port dynamic to inherit port from render

--- trackr/trackr/settings.py
+++ trackr/trackr/settings.py
@@ -86,14 +86,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'trackr.wsgi.application'
 
-if not DEBUG:
     # Prod security
-    SECURE_SSL_REDIRECT = False
-    SESSION_COOKIE_SECURE = True
-    CSRF_COOKIE_SECURE = True
-    SECURE_HSTS_SECONDS = 31536000
-    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-    SECURE_HSTS_PRELOAD = True
+SECURE_SSL_REDIRECT = False
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
